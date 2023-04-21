40053e8e3aef0bffdec777aa66416cd74b5606ed
adri48277 <131199436+adri48277@users.noreply.github.com>
2023-04-21 17:03:55 -0300

    Update index.php

--- index.php
+++ index.php
@@ -8,6 +8,6 @@
 </head>
 <body>
   <h1>Bienvenidos Hola Mundo</h1>
-  <h2>ChoicesPracticas Profesionalizantes2023</2>
+  <h2>ChoicesPracticas Profesionalizantes2023 esto es una prueba</2>
 </body>
 </html>
